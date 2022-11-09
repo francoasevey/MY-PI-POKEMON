217d285e119c5396b2234b9590298d7cc7552f1e
james franco <you@example.com>
2022-11-09 16:10:39 -0300

    reducer pokemons and types

--- client/src/redux/Reducer/index.js
+++ client/src/redux/Reducer/index.js
@@ -1,14 +1,27 @@
 import{
-    
+    GET_ALL_POKEMONS,
+    GET_TYPES
 } from '../Actions/TypeActions'
 
 const initialState = {
-
+    pokemons: [],
+    AllPokemons: [],
+    types: []
 }
 
 function rootReducer(state = initialState, action){
     switch(action.type){
-
+        case GET_ALL_POKEMONS:
+            return{
+                ...state,
+                pokemons: action.payload,
+                AllPokemons: action.payload
+            }
+        case GET_TYPES:
+            return{
+                ...state,
+                types: action.payload
+            }
         default:
             return state
     }
