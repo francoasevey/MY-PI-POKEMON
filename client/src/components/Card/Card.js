b9702a688d986a6c5c6da05236d94d94141bcc40
james franco <you@example.com>
2022-11-10 01:21:36 -0300

    card

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "../Card/Card.module.css";
+function Card({id,name,image,types}) {
+    //let type = types.map((el) => el.name)
+    return(
+        <div>
+            <h3>{id}</h3>
+            <h3>{name}</h3>
+            <img src={image} alt="Img not Found" width="250px" height="250px" />
+            <div>
+            <h4>Type: {types.join(", ")}</h4>
+            </div>
+        </div>
+    )
+}
+export default Card;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,6 +3,9 @@ import NavBar from "../NavBar/NavBar";
 import {useState , useEffect} from "react";
 import {useDispatch,useSelector} from "react-redux";
 import { getPokemons, getTypes } from "../../redux/Actions/index";
+import {Link} from "react-router-dom";
+import Card from '../Card/Card'
+import styles from "../Home/Home.module.css";
 
 export default function Home(){
     const dispatch = useDispatch()
@@ -17,7 +20,22 @@ export default function Home(){
         <div>
             <NavBar/>
             <h1>estas en el Home</h1>
-
+        <div className={styles.cards}>
+               {AllPokemons?.map((el) =>{
+                 return(
+                    <div className="Pokemons" key={el.id}>
+                          <Link to={"/Detail/" + el.id} key={el.id}>
+                          <Card id={el.id} 
+                          name={el.name}
+                          image={el.image? el.image: el.image}
+                          types={el.types}
+                          key={el.id}
+                            />
+                            </Link>
+                    </div>
+                    )
+                })}
+        </div>
         </div>
     )
 }
