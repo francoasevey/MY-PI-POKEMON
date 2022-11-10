bab0fd870b3307fce9ab1ed9d7f8c20cddda7bca
james franco <you@example.com>
2022-11-10 01:22:26 -0300

    card styles

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,10 +3,10 @@ import styles from "../Card/Card.module.css";
 function Card({id,name,image,types}) {
     //let type = types.map((el) => el.name)
     return(
-        <div>
+        <div className={styles.mainContainer}>
             <h3>{id}</h3>
             <h3>{name}</h3>
-            <img src={image} alt="Img not Found" width="250px" height="250px" />
+            <img  className={styles.imagen} src={image} alt="Img not Found" width="250px" height="250px" />
             <div>
             <h4>Type: {types.join(", ")}</h4>
             </div>
