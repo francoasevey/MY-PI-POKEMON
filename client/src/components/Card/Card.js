460d4e823dd178ccfde0bef4238e41af87d944a1
james franco <you@example.com>
2022-11-13 14:30:03 -0300

    texts

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -9,16 +9,14 @@ function Card({id,name,image,types,hp,attack,defense,speed,height,weight}) {
                 <h3>{id}</h3>
                 <h3>{name}</h3>
                 <img  className={styles.imagen} src={image} alt="Img not Found" width="250px" height="250px" />
-                <h3>hp {hp}</h3>
-                <h3>attack {attack}</h3>
-                <h3>defense {defense}</h3>
-                <h3>speed {speed}</h3>
-                <h3>height {height}</h3>
-                <h3>weight {weight}</h3>
+                <h3>Hp {hp}</h3>
+                <h3>Attack {attack}</h3>
+                <h3>Defense {defense}</h3>
+                <h3>Speed {speed}</h3>
+                <h3>Height {height}</h3>
+                <h3>Weight {weight}</h3>
                 <div>
-                <h3 className={styles.p}>
-                {types[1] ? types[0] + " / " + types[1] : types[0]}
-                </h3>
+                <h3 className={styles.p}>Types: {types[1] ? types[0] + " / " + types[1] : types[0]}</h3>
                 </div>
             </div>
         )
