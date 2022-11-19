8d0781a6289f903fa483cbc423a61e9142da6c50
james franco <you@example.com>
2022-11-18 21:52:40 -0300

    random clear

--- client/src/components/RandomPokemon/RandomPokemon.js
+++ client/src/components/RandomPokemon/RandomPokemon.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import {useEffect } from "react";
+import { Link ,useParams} from "react-router-dom";
+import { useSelector,useDispatch } from "react-redux";
+import {cleanPokemonDetail} from '../../redux/Actions/index'
 import styles from '../RandomPokemon/RandomPokemon.module.css';
 
-const RandomPokemon = () => {   
+const RandomPokemon = () => {  
+    const dispatch = useDispatch();
+    const { id } = useParams();
     const pokemon = useSelector(state => state.pokemons);
     const idPokemon = Array.isArray(pokemon) && pokemon.map(el => el.id);
     let randomPokemon = idPokemon[Math.floor(Math.random() * idPokemon.length)];
+    useEffect(() => {
+        return () => {
+          dispatch(cleanPokemonDetail());
+        };
+      }, [dispatch], id);
 
+      const randomPoke = () =>{
+        dispatch(cleanPokemonDetail());
+      }
+      
     return (
         <div>
             <Link to={`/Detail/${randomPokemon}`}>
-                <button className={styles.rndBut}>Random Pokemon</button>
+                <button className={styles.rndBut} onClick={randomPoke}>Random Pokemon</button>
+                
             </Link>
         </div>
     )
